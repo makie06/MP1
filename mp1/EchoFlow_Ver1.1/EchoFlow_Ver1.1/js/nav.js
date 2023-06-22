715fe8d646f9588d37a1725be6cdf59631565aa7
makie06 <114809407+makie06@users.noreply.github.com>
2023-06-22 15:12:27 +0800

    Add files via upload

---
+++ mp1/EchoFlow_Ver1.1/EchoFlow_Ver1.1/js/nav.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("nav-menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
